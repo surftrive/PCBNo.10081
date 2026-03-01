f6a799d0f0dae7deebe6beb27b0be3ce8f6824a5
surftrive <surftrive@gmail.com>
2026-03-01 12:08:18 +0900

    1
    1

--- PCBNo.10081/FPGA/SP0557/synlog.tcl
+++ PCBNo.10081/FPGA/SP0557/synlog.tcl
@@ -1,1 +1,3 @@
-run_tcl -fg SP0557_SP0557_synplify.tcl
+history clear
+run_tcl -fg C:/FPGAwork/PCBNo.10081/FPGA/SP0557/launch_synplify.tcl
+project -close C:/FPGAwork/PCBNo.10081/FPGA/SP0557/SP0557_syn.prj
